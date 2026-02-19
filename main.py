67a6db216ce3187dfead624c73fa3be412e8ec2a
Nine <_EMAIL__GITHUB>
2026-02-19 10:23:55 +0700

    Update project

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./users.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class UserDB(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    age = Column(Integer)
+
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
+users = []
+current_id = 1
 
 @app.get("/")
 def read_root():
@@ -14,11 +35,53 @@ class User(BaseModel):
     name: str
     age: int
 
-
 @app.post("/users/")
 def create_user(user: User):
+    db = SessionLocal()
+
+    new_user = UserDB(name=user.name, age=user.age)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    db.close()
+
     return {
-        "name": user.name,
-        "age": user.age,
-        "status": "Пользователь создан"
+        "id": new_user.id,
+        "name": new_user.name,
+        "age": new_user.age
     }
+
+@app.get("/users/")
+def get_users():
+    db = SessionLocal()
+    users = db.query(UserDB).all()
+    db.close()
+    return users
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, updated_user: User):
+    db = SessionLocal()
+
+    user = db.query(UserDB).filter(UserDB.id == user_id).first()
+
+    if not user:
+        db.close()
+        return {"error": "Пользователь не найден"}
+
+    user.name = updated_user.name
+    user.age = updated_user.age
+
+    db.commit()
+    db.refresh(user)
+    db.close()
+
+    return {
+        "id": user.id,
+        "name": user.name,
+        "age": user.age
+    }
+
+
+
+
